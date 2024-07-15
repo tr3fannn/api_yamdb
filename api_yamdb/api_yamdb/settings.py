e5519ecdbe0924a570c8f60209dd856d1713cbf3
Anton Savchenko <darkflamefox24@gmail.com>
2024-07-15 17:43:12 +0300

    Добавил модели в приложении reviews

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,26 +1,26 @@
 from pathlib import Path
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
-# Application definition
-
 INSTALLED_APPS = [
+    # Django.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Other.
+    'rest_framework',
+    # Local.
+    'api.apps.ApiConfig',
+    'reviews.apps.ReviewsConfig',
 ]
 
 MIDDLEWARE = [
@@ -36,6 +36,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'api_yamdb.urls'
 
 TEMPLATES_DIR = BASE_DIR / 'templates'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -54,18 +55,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
-
-# Database
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -82,9 +77,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -94,9 +86,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
 
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,104 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Category(models.model):
+    name = models.CharField(
+        max_length=256,
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.model):
+    name = models.CharField(
+        max_length=256,
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.model):
+    name = models.CharField(
+        max_length=256,
+    )
+    year = models.IntegerField()
+    description = models.TextField(
+        blank=True,
+        null=True,
+        default=None,
+    )
+    category = models.OneToOneField(
+        Category,
+        related_name='titles',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    genre = models.ForeignKey(
+        Genre,
+        related_name='titles',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.model):
+    text = models.TextField()
+    score = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10),
+        ],
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+    )
+    author = models.ForeignKey(
+        User,
+        related_name='reviews',
+        on_delete=models.CASCADE,
+    )
+    title = models.ForeignKey(
+        Title,
+        related_name='reviews',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.text[:10]
+
+
+class Comment(models.model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+    )
+    author = models.ForeignKey(
+        User,
+        related_name='comments',
+        on_delete=models.CASCADE,
+    )
+    review = models.ForeignKey(
+        Review,
+        related_name='comments',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.text[:10]
