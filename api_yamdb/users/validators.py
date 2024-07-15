9eef547700a8a411ef72eefc984bb39590e9e1ca
Ярослав Богданов <givik7788@gmail.com>
2024-07-15 18:43:49 +0300

    +app 'Users', +model User, +admin for User

---
+++ api_yamdb/users/validators.py
@@ -0,0 +1,13 @@
+from django.core.exceptions import ValidationError
+
+
+def validate_username(value):
+    if not isinstance(value, str):
+        raise ValidationError(
+            'username должен иметь тип str'
+        )
+    if value.lower() == 'me':
+        raise ValidationError(
+            f'username не может быть "{value}"'
+        )
+    return value
