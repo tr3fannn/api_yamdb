6d768f1de63bc5b8d773201f376914c3ff976f10
tr3fannn <146739927+tr3fannn@users.noreply.github.com>
2024-07-18 17:43:52 +0300

    Feature/extras (#8)
    * new app: api, reviews
    
    * +app 'Users', +model User, +admin for User (#2)
    
    * +app 'Users', +model User, +admin for User
    
    * исправил админку
    
    * удалил лишние файлы
    
    * Добавил модели в приложении reviews (#1)
    
    Co-authored-by: Anton Savchenko <darkflamefox24@gmail.com>
    
    * Немножко порядка pep8
    
    * Скрипт конвертации csv в json фикстуры
    
    * Вьюсеты, url'ы
    
    * Auth/token/category/genre
    
    * +проверка прав
    
    * Доработка проекта
    
    * работает - не трогай
    
    ---------
    
    Co-authored-by: AnthonyFox204 <157055752+AnthonyFox204@users.noreply.github.com>
    Co-authored-by: Anton Savchenko <darkflamefox24@gmail.com>
    Co-authored-by: apetrushishin <apetrushishin@ivi.ru>
    Co-authored-by: Александр <MrDarwin1859@yandex.ru>

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,166 @@
+from django.urls import include, path
+
+from .views import (
+    CategoryViewSet,
+    CommentViewSet,
+    CreateUserView,
+    GenreViewSet,
+    ObtainTokenView,
+    ReviewViewSet,
+    TitleViewSetDetail,
+    TitleViewSetListCreate,
+    UserViewSet,
+)
+
+app_name = 'api_v1'
+
+users = [
+    path('auth/signup/', CreateUserView.as_view()),
+    path('auth/token/', ObtainTokenView.as_view()),
+    path(
+        'users/',
+        UserViewSet.as_view(
+            {
+                'get': 'list',
+                'post': 'create',
+            }
+        ),
+    ),
+    path(
+        'users/<str:username>/',
+        UserViewSet.as_view(
+            {
+                'get': 'retrieve',
+                'patch': 'partial_update',
+                'delete': 'destroy',
+            }
+        ),
+    ),
+    path(
+        'users/me/',
+        UserViewSet.as_view(
+            {
+                'get': 'retrieve',
+                'patch': 'partial_update',
+            }
+        ),
+    ),
+]
+
+category = [
+    path(
+        'categories/',
+        CategoryViewSet.as_view(
+            {
+                'get': 'list',
+                'post': 'create',
+            }
+        ),
+    ),
+    path(
+        'categories/<slug:cat_slug>/',
+        CategoryViewSet.as_view(
+            {
+                'patch': 'partial_update',
+                'delete': 'destroy',
+            }
+        ),
+    ),
+]
+
+genre = [
+    path(
+        'genres/',
+        GenreViewSet.as_view(
+            {
+                'get': 'list',
+                'post': 'create',
+            }
+        ),
+    ),
+    path(
+        'genres/<slug:gen_slug>/',
+        GenreViewSet.as_view(
+            {
+                'patch': 'partial_update',
+                'delete': 'destroy',
+            }
+        ),
+    ),
+]
+
+comment = [
+    path(
+        'titles/<int:title_id>/reviews/<int:review_id>/comments/',
+        CommentViewSet.as_view(
+            {
+                'get': 'list',
+                'post': 'create',
+            }
+        ),
+    ),
+    path(
+        'titles/<int:title_id>/'
+        'reviews/<int:review_id>/'
+        'comments/<int:comment_id>/',
+        CommentViewSet.as_view(
+            {
+                'get': 'retrieve',
+                'patch': 'partial_update',
+                'delete': 'destroy',
+            }
+        ),
+    ),
+]
+
+review = [
+    path(
+        'titles/<int:title_id>/reviews/',
+        ReviewViewSet.as_view(
+            {
+                'get': 'list',
+                'post': 'create',
+            }
+        ),
+    ),
+    path(
+        'titles/<int:title_id>/reviews/<int:review_id>/',
+        ReviewViewSet.as_view(
+            {
+                'get': 'retrieve',
+                'patch': 'partial_update',
+                'delete': 'destroy',
+            }
+        ),
+    ),
+]
+
+title = [
+    path(
+        'titles/',
+        TitleViewSetListCreate.as_view(
+            {
+                'get': 'list',
+                'post': 'create',
+            }
+        ),
+    ),
+    path(
+        'titles/<int:title_id>/',
+        TitleViewSetDetail.as_view(
+            {
+                'get': 'retrieve',
+                'patch': 'partial_update',
+                'delete': 'destroy',
+            }
+        ),
+    ),
+]
+urlpatterns = [
+    path('', include(users)),
+    path('', include(category)),
+    path('', include(genre)),
+    path('', include(comment)),
+    path('', include(review)),
+    path('', include(title)),
+]
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,26 +1,28 @@
+from datetime import timedelta
 from pathlib import Path
-
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
-# Application definition
-
 INSTALLED_APPS = [
+    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Other
+    'rest_framework',
+    'rest_framework_simplejwt',
+    # Local
+    'api',
+    'reviews',
 ]
 
 MIDDLEWARE = [
@@ -36,6 +38,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'api_yamdb.urls'
 
 TEMPLATES_DIR = BASE_DIR / 'templates'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -54,18 +57,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
-
-# Database
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -82,9 +79,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -95,9 +89,24 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
+
+AUTH_USER_MODEL = 'reviews.User'
